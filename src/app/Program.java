aa977126b916c2c7c0419b536439595bd5acac47
Ezandro Bueno <ezandrobueno@yahoo.com.br>
2021-03-29 19:07:03 -0300

    Finalizado a aula sobre Generics

---
+++ src/app/Program.java
@@ -0,0 +1,16 @@
+package app;
+
+import util.List;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Double> list = new List<>();
+		
+		list.add(6.4);
+		list.add(1.2);
+		System.out.println(list);
+
+	}
+	
+}
